257159192c7c32016f96a7776ec5692a4d8edf1d
Tbjamie <144114217+Tbjamie@users.noreply.github.com>
2024-03-07 11:55:11 +0100

    server added + nav gefixt

---
+++ server.js
@@ -0,0 +1,80 @@
+// dit is een boilerplate voor een node.js webserver met alle basis die je nodig hebt om je webserver aan de praat te krijgen
+
+// Add info from .env file to process.env
+require("dotenv").config();
+
+// Initialise Express webserver
+const express = require("express");
+const { disconnect } = require("process");
+const app = express();
+// const {
+//   ServerApiVersion,
+//   ObjectId,
+//   MongoClient,
+// } = require("mongodb");
+
+// const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+// const client = new MongoClient(uri, {
+//   serverApi: {
+//     version: ServerApiVersion.v1,
+//     strict: true,
+//     deprecationErrors: true,
+//   }
+// })
+
+// client.connect()
+//     .then((res) => {
+//         console.log("Database connection established")
+//     })
+//     .catch((err) => {
+//         console.log(`Database connection error - ${err}`)
+//         console.log(`For uri - ${uri}`)
+//     })
+
+// const db = client.db(process.env.DB_NAME)
+// const collection = db.collection(process.env.DB_COLLECTION)
+
+app
+  .use(express.urlencoded({ extended: true })) // middleware to parse form data from incoming HTTP request and add form fields to req.body
+  .use(express.static("./static")) // Allow server to serve static content such as images, stylesheets, fonts or frontend js from the directory named static
+  .set("view engine", "ejs") // Set EJS to be our templating engine
+  .set("views", "view") // And tell it the views can be found in the directory named view
+  .get("/", welcome)
+  .get("/discover", discover)
+//   .post("/sign-up", signUp);
+
+app.listen(`${8006}`, () => {
+  console.log(
+    `Running on port ${8006}`
+  );
+});
+
+// async function getUsers(req, res) {
+//     users = await collection.find().toArray()
+
+//     console.log(users)
+//     res.render('test.ejs', {users: users})
+// }
+
+function welcome(req, res) {
+    res.render("welcome.ejs")
+}
+
+function discover(req, res) {
+    res.render("discover.ejs")
+}
+
+// Middleware to handle not found errors - error 404
+app.use((req, res) => {
+  console.error("404 error at URL: " + req.url);
+  if(res.status(404)) {
+      res.render("404page.ejs")
+      console.log("TEST")
+  }
+});
+
+// Middleware to handle server errors - error 500
+app.use((err, req, res) => {
+  console.error(err.stack);
+  res.status(500).send("500: server error");
+});
--- static/js/discover.js
+++ static/js/discover.js
@@ -4,6 +4,12 @@ const filterButton = document.querySelector(
   "main section:first-of-type .flexdiv button"
 );
 const closeButtonFilter = document.querySelector(".cross");
+const navButtonHome = document.querySelector("nav ul li a:first-of-type")
+const navButtonDiscover = document.querySelector("nav ul li a:nth-of-type(2)")
+const navButtonCommunity = document.querySelector("nav ul li a:nth-of-type(3)")
+const navButtonForum = document.querySelector("nav ul li a:nth-of-type(4)")
+const navButtonAccount = document.querySelector("nav ul li a:last-of-type")
+const navButtons = document.querySelectorAll("nav ul li")
 
 for (input of document.querySelectorAll("input[type=range]")) {
   actualizarInput(input);
@@ -50,3 +56,56 @@ closeButtonFilter.addEventListener("click", () => {
   //   });
   // });
 });
+
+const activeNavPage = () => {
+
+  // navButtonHome.addEventListener("click", () => {
+  //   navButtonHome.classList.add("activeNavPage")
+  //   navButtonDiscover.classList.remove("activeNavPage")
+  //   navButtonCommunity.classList.remove("activeNavPage")
+  //   navButtonForum.classList.remove("activeNavPage")
+  //   navButtonAccount.classList.remove("activeNavPage")
+  // })
+
+  // navButtonDiscover.addEventListener("click", () => {
+  //   navButtonHome.classList.remove("activeNavPage")
+  //   navButtonDiscover.classList.add("activeNavPage")
+  //   navButtonCommunity.classList.remove("activeNavPage")
+  //   navButtonForum.classList.remove("activeNavPage")
+  //   navButtonAccount.classList.remove("activeNavPage")
+  // })
+
+  // navButtonCommunity.addEventListener("click", () => {
+  //   navButtonHome.classList.remove("activeNavPage")
+  //   navButtonDiscover.classList.remove("activeNavPage")
+  //   navButtonCommunity.classList.add("activeNavPage")
+  //   navButtonForum.classList.remove("activeNavPage")
+  //   navButtonAccount.classList.remove("activeNavPage")
+  // })
+
+  // navButtonForum.addEventListener("click", () => {
+  //   navButtonHome.classList.remove("activeNavPage")
+  //   navButtonDiscover.classList.remove("activeNavPage")
+  //   navButtonCommunity.classList.remove("activeNavPage")
+  //   navButtonForum.classList.add("activeNavPage")
+  //   navButtonAccount.classList.remove("activeNavPage")
+  // })
+
+  // navButtonAccount.addEventListener("click", () => {
+  //   navButtonHome.classList.remove("activeNavPage")
+  //   navButtonDiscover.classList.remove("activeNavPage")
+  //   navButtonCommunity.classList.remove("activeNavPage")
+  //   navButtonForum.classList.remove("activeNavPage")
+  //   navButtonAccount.classList.add("activeNavPage")
+  // })
+
+  navButtons.forEach(button => {
+    console.log(button)
+      button.addEventListener("click", () => {
+        button.classList.add("activeNavPage")
+      })
+      button.classList.remove("activeNavPage")
+  })
+}
+
+activeNavPage();
