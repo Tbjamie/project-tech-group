9e602fa2d3cab7ca5aa0ce39e39d28a163336896
KeyshaManucan <150794944+KeyshaManucan@users.noreply.github.com>
2024-03-15 03:19:52 +0100

    Werkende slider voor New Releases

--- static/js/home.js
+++ static/js/home.js
@@ -4,7 +4,65 @@ var swiper = new Swiper(".mySwiper", {
     dynamicBullets: true,
   },
   autoplay: {
-    delay: 10000, // Set the delay to 5000 milliseconds (5 seconds)
-    disableOnInteraction: false, // Allow autoplay to continue even if the user interacts with the slider
+    delay: 10000, 
+    disableOnInteraction: false, 
   },
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+  const sliderWrapper = document.querySelector(".slider-wrapper");
+  const slides = document.querySelectorAll(".slider-slide");
+  const slideWidth = slides[0].offsetWidth;
+
+  let slideIndex = 0;
+  let startX = 0;
+  let endX = 0;
+
+  function showSlide(index) {
+    const offset = -slideWidth * index;
+    sliderWrapper.style.transform = `translateX(${offset}px)`;
+  }
+
+  function showNextSlide() {
+    slideIndex++;
+    if (slideIndex >= slides.length) {
+      slideIndex = 0;
+    }
+    showSlide(slideIndex);
+  }
+
+  function showPreviousSlide() {
+    slideIndex--;
+    if (slideIndex < 0) {
+      slideIndex = slides.length - 1;
+    }
+    showSlide(slideIndex);
+  }
+
+  sliderWrapper.addEventListener("mousedown", (e) => {
+    startX = e.clientX;
+  });
+
+  sliderWrapper.addEventListener("mouseup", (e) => {
+    endX = e.clientX;
+    if (startX - endX > 50) {
+      showNextSlide();
+    } else if (endX - startX > 50) {
+      showPreviousSlide();
+    }
+  });
+
+  sliderWrapper.addEventListener("touchstart", (e) => {
+    startX = e.touches[0].clientX;
+  });
+
+  sliderWrapper.addEventListener("touchend", (e) => {
+    endX = e.changedTouches[0].clientX;
+    if (startX - endX > 50) {
+      showNextSlide();
+    } else if (endX - startX > 50) {
+      showPreviousSlide();
+    }
+  });
+});
+
