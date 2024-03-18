7204f8babb785b0d54397245fb5a81509cf70b85
camil1404 <camilmgilliad@gmail.com>
2024-03-18 11:45:40 +0100

    svgs of genres added and cross next to active filters

--- static/js/discover.js
+++ static/js/discover.js
@@ -142,6 +142,24 @@ fetch("/static/json/games.json")
           let filterItem = document.createElement("button");
           filterItem.innerText = genre;
           filterItem.dataset.genre = genre; // Voeg dataset-attribuut toe voor identificatie
+          // Voeg de SVG icon toe aan de button
+          const svgIcon = document.createElementNS(
+            "http://www.w3.org/2000/svg",
+            "svg"
+          );
+          svgIcon.setAttribute("class", "cross");
+          svgIcon.setAttribute("viewBox", "0 0 24 24");
+          svgIcon.setAttribute("xml:space", "preserve");
+          const path = document.createElementNS(
+            "http://www.w3.org/2000/svg",
+            "path"
+          );
+          path.setAttribute(
+            "d",
+            "M5.3,18.7C5.5,18.9,5.7,19,6,19s0.5-0.1,0.7-0.3l5.3-5.3l5.3,5.3c0.2,0.2,0.5,0.3,0.7,0.3s0.5-0.1,0.7-0.3c0.4-0.4,0.4-1,0-1.4L13.4,12l5.3-5.3c0.4-0.4,0.4-1,0-1.4s-1-0.4-1.4,0L12,10.6L6.7,5.3c-0.4-0.4-1-0.4-1.4,0s-0.4,1,0,1.4l5.3,5.3l-5.3,5.3C4.9,17.7,4.9,18.3,5.3,18.7z"
+          );
+          svgIcon.appendChild(path);
+          filterItem.appendChild(svgIcon);
           filterItem.addEventListener("click", () => {
             // Deselecteer het genre wanneer de filterknop wordt geklikt
             genreFilterButton.classList.remove("selected");
