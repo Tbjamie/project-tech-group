864d1d789bbe92d317c993551306c0908f306277
Tbjamie <144114217+Tbjamie@users.noreply.github.com>
2024-03-29 14:14:40 +0100

    + pop-up gemaakt voor confirmation op editAcc page

--- static/js/editAccount.js
+++ static/js/editAccount.js
@@ -1,1 +1,17 @@
 const cancelButton = document.querySelector('#cancelButton')
+const mainEl = document.querySelector('main')
+const okButton = document.querySelector('#account .cancelChangeMessage div button:first-of-type')
+const dontCancelButton = document.querySelector('#account .cancelChangeMessage div button:last-of-type')
+const cancelMessage = document.querySelector('#account .cancelChangeMessage')
+
+cancelButton.addEventListener('click', () => {
+    cancelMessage.style.transform = 'translateY(0)'
+})
+
+okButton.addEventListener('click', () => {
+    history.back()
+})
+
+dontCancelButton.addEventListener('click', () => {
+    cancelMessage.style.transform = 'translateY(100rem)'
+})
