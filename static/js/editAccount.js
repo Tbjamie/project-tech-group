3feaf01df891a004e85e6cedfc61a8a2492153d4
Tbjamie <144114217+Tbjamie@users.noreply.github.com>
2024-03-29 13:12:38 +0100

    + Loading screen een component gemaakt

---
+++ static/js/editAccount.js
@@ -0,0 +1,1 @@
+const cancelButton = document.querySelector('#cancelButton')
--- static/js/loadingScreen.js
+++ static/js/loadingScreen.js
@@ -1,10 +1,7 @@
-const bodyElement = document.body;
+const mainElement = document.querySelector('main')
+const loadingScreen = document.querySelector('#loadingScreen')
 
-document.addEventListener("DOMContentLoaded", function () {
-  const loadingScreen = document.querySelector(".loadingScreen");
-
-  setTimeout(function () {
-    loadingScreen.style.display = "none";
-    bodyElement.style.overflowY = "scroll";
-  }, 2000);
-});
+document.addEventListener('DOMContentLoaded', () => {
+  loadingScreen.style.display = 'none'
+  mainElement.style.overflowY = 'scroll'
+})
