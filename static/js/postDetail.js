c07ce641485207beb8e2c5bba15ef9da1f5c1ad1
Tbjamie <144114217+Tbjamie@users.noreply.github.com>
2024-04-06 19:04:52 +0200

    + Postdetail page comments weergeven

---
+++ static/js/postDetail.js
@@ -0,0 +1,11 @@
+const commentCount = document.querySelector('#postDetail section:nth-of-type(2) > p:last-of-type')
+
+const commentCountTextChange = () => {
+        if(commentCount.innerText == 1) {
+            commentCount.innerText = `${commentCount.innerText} Comment`
+        } else {
+            commentCount.innerText = `${commentCount.innerText} Comments`
+        }
+    }
+
+commentCountTextChange()
